bdc084fd807a326106c0ca0f1a57678899292fe7
Sammy Arenson <52387656+FeatheredSnak3@users.noreply.github.com>
2024-06-29 13:33:17 -0700

    Should be functional

--- java/com/aa/act/interview/org/Organization.java
+++ java/com/aa/act/interview/org/Organization.java
@@ -1,6 +1,10 @@
 package com.aa.act.interview.org;
 
 import java.util.Optional;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicInteger;
+
 
 public abstract class Organization {
 
@@ -19,10 +23,40 @@ public abstract class Organization {
      * @param title
      * @return the newly filled position or empty if no position has that title
      */
+    
+     //START of my code
+     private AtomicInteger nextEmployeeId = new AtomicInteger(1); //Initialize unique ID at 1
+
     public Optional<Position> hire(Name person, String title) {
-        //your code here
+        Queue<Position> queue = new LinkedList<>();
+
+        queue.add(root); //Start from Root Position
+        
+        while (!queue.isEmpty()) {
+            Position currPosition = queue.remove(); //Remove head of Queue 
+            
+            //Determine if current title matches title parameter
+            if(currPosition.getTitle() == title) {
+                Employee newEmployee = new Employee(createUniqueId(), person);//New Employee for current person 
+
+                currPosition.setEmployee(Optional.of(newEmployee));//Assign Employee to the position
+                
+                return Optional.of(currPosition); //Return matching position
+            }
+            
+            //Iterate over each Position object within DirectReports
+            for (Position directReport : currPosition.getDirectReports()) { 
+                queue.add(directReport); //Add direct reports to the end of the queue
+            }  
+        }
+
         return Optional.empty();
     }
+
+    private int createUniqueId() {
+        return nextEmployeeId.getAndIncrement();
+    }//END of my code
+
 
     @Override
     public String toString() {
